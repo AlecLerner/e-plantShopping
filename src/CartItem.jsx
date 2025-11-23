01544e19e0206e40b78ed5dae4d17deb1961570d
AlecLerner <AlecLerner@yahoo.com>
2025-11-23 13:51:08 -0500

    CartItems component

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,48 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach((item) => {
+        let cost = parseFloat( item.cost.substring(1));
+        total += cost * item.quantity;  
+    });
+    return total;
   };
+
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
-
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if ( item.quantity > 1) {
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    }
+    if ( item.quantity == 0 ) {
+        dispatch(removeItem({ name: item.name }));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem({ name: item.name }));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let totalCost = 0;
+    cart.forEach((item) => {
+        let cost = parseFloat( item.cost.substring(1));
+        totalCost += cost * item.quantity;  
+    });
+    return totalCost;    
   };
 
   return (
